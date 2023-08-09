d59c643f6a39405285e56bf9259eadc086c0ea51
Mxolisi Zondi <mxolisi.zondi@iwyze.co.za>
2023-08-09 14:28:14 +0200

    default_start

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+
+def monthly_challenge(request, month):
+    if (month == 'january'):
+        return HttpResponse("This january!!")
+    elif (month == 'february'):
+        return HttpResponse("Wak for at least 20 minutes every day")
+    else:
+        return HttpResponseNotFound("Page not found")
